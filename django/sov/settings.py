ba09ccfff30ffe8fb16ff5f9925ec8f13663dab1
kotrfa <kotrfa@gmail.com>
2015-10-10 12:23:09 +0200

    secret key obfusc

--- django/sov/settings.py
+++ django/sov/settings.py
@@ -20,7 +20,7 @@ ADMIN_MEDIA_PREFIX = '/media/admin/'
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kz(3_kt_tres(^jbbfqr6qwoyfhign&1vlu@t@kr48+sz10a&f'
+SECRET_KEY = open("/var/www/sovazlutice/secret_key.txt").read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
