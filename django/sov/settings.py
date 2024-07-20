7c787732722170bdaeefbca91b2e65b3b1c9bb99
Daniel Hnyk <hnykda@gmail.com>
2024-07-20 13:55:41 +0200

    db

--- django/sov/settings.py
+++ django/sov/settings.py
@@ -73,9 +73,10 @@ WSGI_APPLICATION = 'sov.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, "..", 'db.sqlite3'),
     }
 }
+print(DATABASES, MEDIA_ROOT, os.path.exists(MEDIA_ROOT))
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
